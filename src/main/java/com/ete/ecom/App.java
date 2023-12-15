1560ef62eebbdace3c2b8994e1e0bd12cd8d1952
radhi <radhikabhatt.ec@gmail.com>
2023-12-15 10:14:20 +0300

    Student objects created

--- src/main/java/com/ete/ecom/App.java
+++ src/main/java/com/ete/ecom/App.java
@@ -1,6 +1,8 @@
 package com.ete.ecom;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 /**
@@ -15,5 +17,25 @@ public class App
         
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         System.out.println(sessionFactory);
+//        System.out.println(sessionFactory.isClosed());
+        
+        //Creating Student Object
+        Student std = new Student();
+        
+        std.setId(102);
+        std.setName("SarRadhika");
+        std.setCity("NoidaWest");
+        
+        System.out.println(std);
+        
+        Session session = sessionFactory.openSession();
+        
+        Transaction transaction = session.beginTransaction();
+        
+        session.save(std);
+         
+        transaction.commit();
+        
+        session.close();
     }
 }
